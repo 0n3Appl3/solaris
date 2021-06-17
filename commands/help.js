4f3e279f9d4075b35e3fd3294eea591bcf8867a3
Jedd Lupoy <jedd.lupoy@gmail.com>
2021-06-17 17:29:47 +1200

    Added conversational feature
    Solaris can now have a natural conversation with a member. It can also recognise words and provide useful tutorial links

--- commands/help.js
+++ commands/help.js
@@ -3,10 +3,11 @@ module.exports = {
     description: 'Help command.',
     execute(message) {
         const Discord = require('discord.js');
+        const { bot_info } = require('../config.json');
         const embed = new Discord.MessageEmbed()
         .setColor('#db2b39')
         .setTitle('Help Page')
-        .setDescription('Hi there! I am Solaris, your virtual assistant on Appl3 PvP. I have very limited functionality right now, but I can provide useful links and info about the server simply by executing commands.')
+        .setDescription(`Hi there! I am ${bot_info.name}, your virtual assistant on Appl3 PvP. I have very limited functionality right now, but I can provide useful links and info about the server simply by executing commands.`)
         .addFields(
             { name: 'General', value: '`/help` Shows this page\n`/info` Displays basic information about the server\n`/ip` Displays the server IP and version\n`/socials` Visit our socials', inline: false },
             { name: 'Tutorials', value: '`/tutorials` Shows the playlist of all tutorial videos\n`/claims` How to create and manage claims\n`/market` How to use the market system\n`/homes` How to create and manage homes\n`/remtp` How to remove teleportation cooldowns\n`/morehomes` How to add more homes', inline: false },
--- index.js
+++ index.js
@@ -7,29 +7,95 @@ client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 const { prefix, token, bot_info } = require('./config.json');
+const fetch = require('node-fetch').default;
 
+// General keywords for queries.
+const help = ['help'];
+const how = ['how'];
+// Keywords to detect claim queries.
+const claimHelp = ['claim'];
+// Keywords to detect market queries.
+const marketHelp = ['market'];
+const shopHelp = ['shop'];
+const sell = ['sell', 'item'];
+const buy = ['buy', 'item'];
+// Keywords to detect home queries.
+const homeHelp = ['home'];
+const more = ['more'];
+// Keywords to detect remtp queries.
+const cooldownHelp = ['cooldown'];
+const teleport = ['teleport'];
+const tp = ['tp'];
+
+// Initialise the bot.
 client.once('ready', () => {
 	console.log(bot_info.name + ' is ready!');
     client.user.setActivity('/help', { type: 'LISTENING' });
 });
 
+// Bot will now be online.
 client.login(token);
 
+// Load all commands.
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
 }
 
-client.on('message', message => {
+// Even triggers when a message is sent to the chat.
+client.on('message', async message => {
+    if (contextMatch(message.content, help) || contextMatch(message.content, how)) {
+        // Someone is asking about claiming.
+        if (contextMatch(message.content, claimHelp)) {
+            showEmbed(message, 'I know a tutorial that could help you out. Gimme a sec!', 'claims');
+        }
+        // Someone is asking about the market.
+        if (contextMatch(message.content, marketHelp)
+            || contextMatch(message.content, shopHelp)
+            || contextMatch(message.content, sell)
+            || contextMatch(message.content, buy)) {
+            showEmbed(message, 'I can help you buy and sell with confidence! Hold on...', 'market');
+        }
+        // Someone is asking about homes.
+        if (contextMatch(message.content, homeHelp)) {
+            // Are they asking about adding more homes?
+            if (contextMatch(message.content, more)) {
+                showEmbed(message, 'I can help you with that. Here you go!', 'morehomes');
+            } else {
+                showEmbed(message, 'I hope this tutorial will help you out, should be pretty simple.', 'homes');
+            }
+        }
+        // Someone is asking about cooldowns.
+        if (contextMatch(message.content, cooldownHelp)
+            && (contextMatch(message.content, teleport) || contextMatch(message.content, tp))) {
+            showEmbed(message, 'I know this one! This tutorial will show you how to remove it.', 'remtp');
+        }
+        return message.channel.stopTyping(true);
+    }
+
+    // Bot will converse with the member who pinged them.
+    if (message.mentions.has(client.user) && !message.author.bot) {
+        message.channel.startTyping();
+        fetch(`https://api.monkedev.com/fun/chat?msg=${message.content.replace(bot_info.mention, '')}&uid=${message.author.id}&key=qOSvE5VfX82GHIuEk5r0lAE3y`)
+        .then(response => response.json())
+        .then(data => {
+            // Send message to the chat.
+            message.channel.send(data.response);
+        }).catch(() => {
+            message.channel.send('Could not fetch response!');
+        });
+        return message.channel.stopTyping(true);
+    }
+    
+    // Chat as normal if member is not entering a command.
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
+    // Set up command execution.
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const commandName = args.shift().toLowerCase();
 
     if (!client.commands.has(commandName)) return;
     const command = client.commands.get(commandName);
-
-    // if (!args.length) { }
 
     try {
         command.execute(message, args);
@@ -38,3 +104,27 @@ client.on('message', message => {
         message.reply('There was an issue executing that command!');
     }
  });
+
+ // Checks if a message contains all the assigned keywords.
+ function contextMatch(msg, array) {
+    const keywords = array;
+    let i = 0;
+
+    for (i = 0; i < keywords.length; i++) {
+        if (!msg.includes(keywords[i])) {
+            // A keyword was not found in the message.
+            return false;
+        }
+    }
+    // All keywords were found in the message.
+    return true;
+ }
+
+ // Executes a command and pings the member who asked for it.
+ function showEmbed(message, reply, cmd) {
+    message.channel.startTyping();
+    message.reply(reply);
+    message.channel.stopTyping(true);
+    message.channel.startTyping();
+    client.commands.get(cmd).execute(message);
+ }
