884262d4d459d79010a1c59198ce47ff4cd21bb8
Jedd Lupoy <jedd.lupoy@gmail.com>
2022-02-02 20:14:46 +1300

    Added twitter integration
    Posts new Appl3 PvP tweets to a set text channel on the Discord server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const mc = require('minecraft_head');
+const Twit = require('twit');
 const yaml = require('js-yaml');
 
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES", "DIRECT_MESSAGE_REACTIONS", "GUILD_MESSAGE_REACTIONS"], partials: ["MESSAGE", "CHANNEL", "REACTION"] });
@@ -8,7 +9,7 @@ client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-const { prefix, token, chatKey, bot_info, colours, market_channel } = require('./config.json');
+const { prefix, token, chatKey, bot_info, colours, twitter, chats, market_channel } = require('./config.json');
 const fetch = require('node-fetch').default;
 
 // General keywords for queries.
@@ -32,10 +33,57 @@ const tp = ['tp'];
 // Number of seconds between checking new market items.
 const shopInterval = 4;
 
+// Create a new instance of Twit to connect to Twitter application.
+const twit = new Twit({
+    consumer_key: twitter.api_key,
+    consumer_secret: twitter.api_key_secret,
+    access_token: twitter.access_token,
+    access_token_secret: twitter.access_token_secret,
+});
+
 // Initialise the bot.
 client.once('ready', () => {
 	console.log(bot_info.name + ' is ready!');
+    const stream = twit.stream('statuses/filter', { follow: [twitter.user] });
     client.user.setActivity('-help', { type: 'LISTENING' });
+
+    // Post tweet on the Discord server chat.
+    stream.on('tweet', function(tweet) {
+        const channelExists = client.channels.cache.some(c => c.name === chats.twitter_channel);
+
+        // Specified channel needs to exist and is not a retweet or reply to post.
+        if (channelExists && !isReply(tweet)) {
+            const channel = client.channels.cache.find(c => c.name === chats.socials_chat_name);
+            // Form the link to the tweet.
+            const link = 'https://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id_str;
+            const followLink = 'https://twitter.com/intent/follow?screen_name=' + tweet.user.screen_name;
+            
+            // Create the button to able to view tweet.
+            const row = new Discord.MessageActionRow()
+            .addComponents(
+                new Discord.MessageButton()
+                .setLabel('View Tweet')
+                .setStyle('LINK')
+                .setURL(link),
+                new Discord.MessageButton()
+                .setLabel('Follow')
+                .setStyle('LINK')
+                .setURL(followLink),
+            );
+            // Create the embed for previewing the tweet.
+            const embed = new Discord.MessageEmbed()
+            .setColor(colours.default)
+            .setAuthor(tweet.user.name + ' (@' + tweet.user.screen_name + ')', tweet.user.profile_image_url_https, 'https://twitter.com/' + tweet.user.screen_name)
+            .setDescription(tweet.text)
+            .setFooter('Twitter via Solaris', 'https://images-ext-1.discordapp.net/external/bXJWV2Y_F3XSra_kEqIYXAAsI3m1meckfLhYuWzxIfI/https/abs.twimg.com/icons/apple-touch-icon-192x192.png')
+            .setTimestamp();
+            // If tweet contains an image, attach to the embed.
+            if (tweet.entities.media != undefined) {
+                embed.setImage(tweet.entities.media[0].media_url_https);
+            }
+            channel.send({ embeds: [embed], components: [row] });
+        }
+    });
 
     setInterval(() => {
         // Get the latestitem.yml file from the server.
@@ -116,16 +164,15 @@ client.on('messageCreate', async message => {
 
     // Bot will converse with the member who pinged them.
     if (message.mentions.has(client.user) && !message.author.bot) {
-        message.channel.startTyping();
+        message.channel.sendTyping();
         fetch(`https://api.monkedev.com/fun/chat?msg=${message.content.replace(bot_info.mention, '')}&uid=${message.author.id}&key=${chatKey}`)
         .then(response => response.json())
         .then(data => {
             // Send message to the chat.
-            message.channel.send({ content: data.response });
+            return message.channel.send({ content: data.response });
         }).catch(() => {
-            message.channel.send({ content: 'Could not fetch response!' });
+            return message.channel.send({ content: 'Could not fetch response!' });
         });
-        return message.channel.stopTyping(true);
     }
     
     // Chat as normal if member is not entering a command.
@@ -167,3 +214,13 @@ client.on('messageCreate', async message => {
     message.channel.sendTyping();
     client.commands.get(cmd).execute(message);
  }
+
+ function isReply(tweet) {
+    if (tweet.retweeted_status ||
+        tweet.in_reply_to_status_id ||
+        tweet.in_reply_to_status_id_str ||
+        tweet.in_reply_to_user_id ||
+        tweet.in_reply_to_user_id_str ||
+        tweet.in_reply_to_screen_name) return true;
+    return false;
+}
